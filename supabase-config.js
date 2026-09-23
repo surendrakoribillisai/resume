6506fa1230dc24a40124a2447d92e890cfc9cb57
surendrakoribillisai <surendrakoribilli@gmail.com>
2026-09-23 16:52:59 +0530

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Paste your Supabase Project URL and Publishable key here.
 // Do NOT put a secret/service_role key in this file.
-window.SUPABASE_URL = https://hjgxzpfbyevisssuteps.supabase.co/rest/v1/;
-window.SUPABASE_PUBLISHABLE_KEY = sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g;
+window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co/rest/v1/";
+window.SUPABASE_PUBLISHABLE_KEY ="sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g";
 
 if (window.SUPABASE_URL.startsWith("http") && !window.SUPABASE_URL.includes("PASTE_PROJECT_URL")) {
   window.supabaseClient = window.supabase.createClient(
