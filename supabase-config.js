960eeb5be5db72c005da04a5a18e64f825f06d6f
surendrakoribillisai <surendrakoribilli@gmail.com>
2026-09-23 20:04:39 +0530

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,12 +1,14 @@
 window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co";
 
-window.SUPABASE_PUBLISHABLE_KEY = "YOUR_PUBLISHABLE_KEY";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g";
 
 try {
   window.supabaseClient = window.supabase.createClient(
     window.SUPABASE_URL,
     window.SUPABASE_PUBLISHABLE_KEY
   );
+
+  console.log("Supabase connected");
 } catch (e) {
   console.error("Supabase connection failed:", e);
   window.supabaseClient = null;
