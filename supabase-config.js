66df6d9f9974e8c7d4ec89685a163cb83474c5ba
surendrakoribillisai <surendrakoribilli@gmail.com>
2026-09-23 19:41:55 +0530

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Paste your Supabase Project URL and Publishable key here.
 // Do NOT put a secret/service_role key in this file.
-window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co";
 
 window.SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g";
