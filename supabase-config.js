c81a4a7feda660591efd4a81d4a7e37a7e6d7edf
surendrakoribillisai <surendrakoribilli@gmail.com>
2026-09-23 19:31:50 +0530

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,11 +1,17 @@
 // Paste your Supabase Project URL and Publishable key here.
 // Do NOT put a secret/service_role key in this file.
-window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co/rest/v1/";
-window.SUPABASE_PUBLISHABLE_KEY ="sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g";
+window.SUPABASE_URL = "https://hjgxzpfbyevisssuteps.supabase.co";
 
-if (window.SUPABASE_URL.startsWith("http") && !window.SUPABASE_URL.includes("PASTE_PROJECT_URL")) {
-  window.supabaseClient = window.supabase.createClient(
-    window.SUPABASE_URL,
-    window.SUPABASE_PUBLISHABLE_KEY
-  );
+window.SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_6rqyiBlM3Mcxo2kyMxXPEQ_KpcZms9g";
+
+if (
+  window.SUPABASE_URL &&
+  window.SUPABASE_PUBLISHABLE_KEY
+) {
+  window.supabaseClient =
+    window.supabase.createClient(
+      window.SUPABASE_URL,
+      window.SUPABASE_PUBLISHABLE_KEY
+    );
 }
